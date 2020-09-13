74e978e621a9b684a2bdbbb27ff4716b46f1ab80
AmadoJunior <amadoelrepalol@gmail.com>
2020-09-12 20:01:58 -0400

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+//Dependencies
+const express = require("express");
+const app = express();
+
+//Middleware
+
+//Testing
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+
+//Routes
+const testRoute = require("/api/routes/testRoute.js");
+app.use("/api/test", testRoute);
+
+//SPA Production
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(__dirname + "/public/"));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + "public/index.html");
+    })
+}
+
+
+//PORT
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server running on port: " + PORT);
+})
